b6611fa79361ddb4e5ff559fb20d5545a72432ff
yilunandrewgao <yilungao@gmail.com>
2025-12-14 19:27:33 +0800

    update startup

--- startup.sh
+++ startup.sh
@@ -3,9 +3,6 @@ sudo apt update
 sudo apt install python3
 sudo apt install python3-pip
 sudo apt install python3.11-venv
-sudo apt-get install git-all
-git clone https://github.com/yilunandrewgao/MtgDiscordTradingBot.git
-cd discordTradingBot
 python3 -m venv venv
 source venv/bin/activate
 python3 -m pip install -r requirements.txt 
